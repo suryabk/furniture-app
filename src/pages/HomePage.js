cb1dda8a756700e9c03a0b75fdecc908dd4faec8
Surya Bhakti Kusuma <surya27bk@gmail.com>
2022-03-28 05:45:59 +0700

    add hooks useScrollAnchor and useModalDOM

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Clients from "../parts/Clients";
 import Header from "../parts/Header";
-import Hero from "../parts/Hero";
+import Hero from "../parts/HomePage/Hero";
 import BrowseRoom from "../parts/HomePage/BrowseRoom";
 import Footer from "../parts/HomePage/Footer";
 import JustArrived from "../parts/HomePage/JustArrived";
 import Sitemap from "../parts/Sitemap";
+import useScrollAnchor from "../helpers/hooks/useScrollAnchor";
+import useModalDOM from "../helpers/hooks/useModalDOM";
 
 export default function HomePage() {
+  useScrollAnchor();
+  useModalDOM();
   return (
     <>
       <Header theme="white" position="absolute" />
--- src/parts/HomePage/Hero.js
+++ src/parts/HomePage/Hero.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
@@ -15,36 +14,36 @@ export default function Hero() {
           membuat ruangan terasa homey
         </h2>
         <div>
-          <Link
-            to="/room"
+          <a
+            href="#browse-the-room"
             className="bg-pink-400 text-black hover:bg-black hover:text-pink-400 rounded-full px-8 py-3 mt-4 inline-block flex-none transition duration-200"
           >
             Explore Now
-          </Link>
+          </a>
         </div>
       </div>
       <div className="w-full inset-0 md:relative md:w-1/2">
         <div className="relative hero-image">
           <div className="overlay inset-0 bg-black opacity-40 z-10"></div>
-          <div className="overlay right-0 bottom-0 md:inset-0">
+          <div className="overlay right-0 bottom-0 md:inset-0 ">
             <button
               className="video hero-cta focus:outline-none z-30 modal-trigger"
-              data-content='<div class="w-screen pb-56 md:w-88 md:pb-56 relative z-50">
-          <div class="absolute w-full h-full">
-            <iframe
-              width="100%"
-              height="100%"
-              src="https://www.youtube.com/embed/3h0_v1cdUIA"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-          </div>
-        </div>'
+              data-content='<div class="w-screen pb-56 md:w-96 md:pb-56 relative z-50">
+              <div class="absolute w-full h-full">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/3h0_v1cdUIA"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>'
             ></button>
           </div>
           <img
-            src="/images/content/image-section-1.png"
+            src="images/content/image-section-1.png"
             alt="hero 1"
             className="absolute inset-0 md:relative w-full h-full object-cover object-center"
           />
